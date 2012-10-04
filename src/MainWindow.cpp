71324becb420358e771f09b3f70336c4b58cc6ea
Romain Goffe <romain.goffe@gmail.com>
2012-10-04 17:51:13 +0200

    cleanup includes

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -43,7 +43,6 @@
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms. 
  */
-#include <iostream>
 
 #include <QtGui>
 #include <QtXml>
